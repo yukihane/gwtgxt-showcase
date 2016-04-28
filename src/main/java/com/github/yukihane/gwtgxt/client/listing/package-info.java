4a43589568bdaec002d58d3e2092ccbb93421b52
yukihane <yukihane.feather@gmail.com>
2016-04-28 19:08:27 +0900

    add package-info.java

---
+++ src/main/java/com/github/yukihane/gwtgxt/client/listing/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 左側に位置する一覧を表示する機能をMVPごとにパッケージ分けして配置しています.
+ */
+package com.github.yukihane.gwtgxt.client.listing;
---
+++ src/main/java/com/github/yukihane/gwtgxt/client/root/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 基底の画面に関わるクラスをMVPそれぞれのパッケージ配下に配置しています.
+ */
+package com.github.yukihane.gwtgxt.client.root;
