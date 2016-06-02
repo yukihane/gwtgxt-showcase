30a08ca5ced3cf0e1bfb6fa1063f96049d24abae
yukihane <yukihane.feather@gmail.com>
2016-06-02 11:59:29 +0900

    取得&出力ボタン

--- src/main/java/com/github/yukihane/gwtgxt/client/Main.java
+++ src/main/java/com/github/yukihane/gwtgxt/client/Main.java
@@ -2,8 +2,13 @@ package com.github.yukihane.gwtgxt.client;
 
 import com.github.yukihane.gwtgxt.client.grid.InlineEditingGridExample;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.sencha.gxt.widget.core.client.button.TextButton;
 import com.sencha.gxt.widget.core.client.container.Viewport;
+import com.sencha.gxt.widget.core.client.event.SelectEvent;
+import com.sencha.gxt.widget.core.client.event.SelectEvent.SelectHandler;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -12,9 +17,25 @@ public class Main implements EntryPoint {
 
     @Override
     public void onModuleLoad() {
+        final TextButton button = new TextButton("get dirty data");
+        button.addSelectHandler(new SelectHandler() {
+            @Override
+            public void onSelect(final SelectEvent event) {
+                actSelection();
+            }
+        });
+
         final Viewport v = new Viewport();
-        v.add(new InlineEditingGridExample());
+        final DockLayoutPanel p = new DockLayoutPanel(Unit.EM);
+        v.add(p);
+        p.addSouth(button, 5.0);
+        p.add(new InlineEditingGridExample());
 
         RootPanel.get().add(v);
+    }
+
+    private void actSelection() {
+        // TODO Auto-generated method stub
+
     }
 }
